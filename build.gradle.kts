3d234199129859f7d682c9385fe0d16224da4792
viceAdmiralGarp <vyacheslavtihonov636@gmail.com>
2024-12-23 21:31:45 +0200

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "com.mmdev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.springframework:spring-core:6.2.1")
+    implementation("org.springframework:spring-context:6.2.1")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
